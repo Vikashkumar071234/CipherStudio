f2ce22ace891d0e809f4570c263e8e4f459ff558
vikash <vsk17369@gmail.com>
2025-10-22 18:54:29 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,39 @@
+// import IDE from "./components/IDE";
+
+// function App() {
+//   return <IDE />;
+// }
+
+// export default App;
+
+
+import { useEffect, useState } from "react";
 import IDE from "./components/IDE";
 
 function App() {
-  return <IDE />;
+  const [projects, setProjects] = useState([]);
+  const apiUrl = process.env.REACT_APP_API_URL;
+
+  // Updated useEffect with apiUrl dependency
+  useEffect(() => {
+    fetch(`${apiUrl}/api/projects`)
+      .then(res => res.json())
+      .then(data => setProjects(data))
+      .catch(err => console.error(err));
+  }, [apiUrl]); // <-- add apiUrl here
+
+  return (
+    <div>
+      <h1>CipherStudio Projects</h1>
+      <ul>
+        {projects.map(p => (
+          <li key={p._id}>{p.projectName}</li>
+        ))}
+      </ul>
+
+      <IDE /> {/* IDE still rendered below */}
+    </div>
+  );
 }
 
 export default App;
