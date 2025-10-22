fc53f6f7889900c17e5b29aa661e7409aa693134
vikash <vsk17369@gmail.com>
2025-10-22 20:15:44 +0530

    Update projects route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,6 @@
 
 // export default App;
 
-
 import { useEffect, useState } from "react";
 import IDE from "./components/IDE";
 
@@ -14,24 +13,23 @@ function App() {
   const [projects, setProjects] = useState([]);
   const apiUrl = process.env.REACT_APP_API_URL;
 
-  // Updated useEffect with apiUrl dependency
   useEffect(() => {
     fetch(`${apiUrl}/api/projects`)
-      .then(res => res.json())
-      .then(data => setProjects(data))
-      .catch(err => console.error(err));
-  }, [apiUrl]); // <-- add apiUrl here
+      .then((res) => res.json())
+      .then((data) => setProjects(data))
+      .catch((err) => console.error(err));
+  }, [apiUrl]);
 
   return (
-    <div>
+    <div style={{ padding: "20px", fontFamily: "Segoe UI, Roboto, sans-serif" }}>
       <h1>CipherStudio Projects</h1>
       <ul>
-        {projects.map(p => (
-          <li key={p._id}>{p.projectName}</li>
+        {projects.map((p) => (
+          <li key={p._id}>{p.projectName || "Untitled Project"}</li>
         ))}
       </ul>
 
-      <IDE /> {/* IDE still rendered below */}
+      <IDE /> {/* IDE component below */}
     </div>
   );
 }
